2dadb14ecc2f84c9b90f36a136744328752d462c
denis <paillerdenis@gmail.com>
2017-03-13 11:15:59 +0100

    initialisation nikonikoWeb 13032017 11H

---
+++ src/main/java/com/tactfactory/nikonikoWeb/NikonikoWebApplication.java
@@ -0,0 +1,12 @@
+package com.tactfactory.nikonikoWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NikonikoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NikonikoWebApplication.class, args);
+	}
+}
